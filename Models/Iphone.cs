f29241aaa243f9212a35bc4a35aa3416053bdaea
Emerson de Oliveira Batista <emerson@engsupport.eti.br>
2023-01-24 11:56:12 -0300

    Desafio POO Finalizado
    Ajustes e adendos realizados com sucesso!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,20 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"[{this.Modelo}] Instalando o app {nomeApp} ...");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Criando um celular Nokia
+Smartphone smart01 = new Nokia("9 1234-5678", "Nokia Z10", "1234567890", 64);
+
+smart01.Ligar();
+smart01.ReceberLigacao();
+smart01.InstalarAplicativo("Facebook");
+
+Console.WriteLine("-------------------------------------");
+// Criando um celular iPhone
+Smartphone smart02 = new Iphone("9 9876-5432", "iPhone X", "9876543210", 128);
+smart02.Ligar();
+smart02.ReceberLigacao();
+smart02.InstalarAplicativo("Telegram");
